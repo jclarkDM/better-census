5f448d1a2d31e90bdbf625ccbd9650900a877b85
alexdevelomark <alex@develomark.com>
2025-08-15 13:44:45 -0400

    fix: :bug: relative path relative to the actual dep

--- etl/init.ts
+++ etl/init.ts
@@ -1,5 +1,6 @@
 import { DuckDBInstance } from "@duckdb/node-api";
+import path from "path";
 
-export const db = await DuckDBInstance.create("./data/census.db");
+export const db = await DuckDBInstance.create(path.join(__dirname, "..", "data", "census.db"));
 
 export const connection = await db.connect();
