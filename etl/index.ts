406b47a5244dd1a866e86c2d493b7a21a89bcda8
alexdevelomark <alex@develomark.com>
2025-08-20 13:54:32 -0400

    refactor: :recycle: combine csv and dat parsing

--- etl/index.ts
+++ etl/index.ts
@@ -1,25 +1,21 @@
 import path from "node:path";
 import glob from "fast-glob";
 import { createLineStream } from "../util/stream";
-import { separateLine } from "../util/csv";
+import { separateLine as separateLineCSV } from "../util/csv";
 import { separateLine as separateLineDat } from "../util/dat";
 import { initializeDB } from "./init";
 
 const BASE_PATH = "./data/raw/";
 const BATCH_SIZE = 4000;
+
 const connection = await initializeDB();
-
-// const ids = await getAllIds();
-// await setupTable(ids);
-// await loadAll();
-
 const ids = await getAllIds();
 await setupTable(ids);
-// await loadAllDat();
+await loadAll();
 
 // 
 
-export async function getAllIds() {
+async function getAllIds() {
   const files = await glob(`${BASE_PATH}/**/*.{csv,dat}`).then((f) => f.map((file) => file.replace(BASE_PATH, "")));
 
   const ids = new Set<string>();
@@ -40,7 +36,7 @@ export async function getAllIds() {
   return ids;
 }
 
-export async function setupTable(ids: Set<string>) {  
+async function setupTable(ids: Set<string>) {  
   const q = `
     create or replace table data (
     id text primary key,
@@ -54,142 +50,120 @@ export async function setupTable(ids: Set<string>) {
   await connection.run(q);
 }
 
-export async function loadAllDat(){
-  const files = await glob(`${BASE_PATH}/**/*.dat`);
-  for(const file of files) {
+async function loadAll() {
+  const files = await glob(`${BASE_PATH}/**/*.{csv,dat}`);
+  for (const file of files) {
+    const fileType = getFileType(file);
+    if (fileType === "unknown") continue;
+
     const index = files.indexOf(file);
     console.log(`Loading ${index + 1}/${files.length}: ${file}`);
-    const csv = Bun.file(file).stream();
-    const lineStream = createLineStream(csv);
-    
-    const firstLine = await lineStream.next();
-    if(!firstLine.value) continue;
-    const columnLine = separateLineDat(firstLine.value);
 
-    const keptIndices = new Set<number>();
+    if (fileType === "dat") await parseDatFile(file);
+    if (fileType === "csv") await parseCsvFile(file);
+  }
+}
 
-    columnLine.forEach((col, index) => {
-      if(col.includes("_E") && col !== "NAME") {
-        keptIndices.add(index);
-      }
-    });
-    
-    const columns = [...keptIndices].map(i => columnLine[i]);
-    console.log("--", columns.length, "columns in", file);
-
-    let i = 0;
-    const chunks: (string | number)[][] = [];
-
-    const limit = 10;
-    let l = 0;
-    for await(const line of lineStream) {
-      if (l > limit) break;
+async function parseDatFile(filePath: string){
+  const fileStream = Bun.file(filePath).stream();
+  const lineStream = createLineStream(fileStream);
+  
+  const firstLine = await lineStream.next();
+  if(!firstLine.value) return;
+  
+  // Select Columns
+  const columnLine = separateLineDat(firstLine.value);
+  const selectedIndices = new Set<number>();
+  columnLine.forEach((col, index) => {
+    if (col.includes("_E")) selectedIndices.add(index);
+  });
+  
+  const selectedColumns = [...selectedIndices].map(i => columnLine[i]);
+  console.log("--", selectedColumns.length, "columns in", filePath);
+  
+  // Insert Rows
+  let rows = 0;
+  const valuesBatch: (string | number)[][] = [];
+  for await (const line of lineStream) {
+    const split = separateLineDat(line);
+    const geoID = split[0]!;
+    const selectedValues = split.filter((_, idx) => selectedIndices.has(idx)).map(parseNumber);
       
-      const split = separateLineDat(line);
-      const chunkLine = split
-        .filter((_, i) => keptIndices.has(i))
-        .map(val => {
-          const asNumber = Number(val);
-          if(isNaN(asNumber)) return "NULL";
-          return asNumber;
-        });
-      const geoId = split[0]! as string;
-      if (!geoId.startsWith("06000")) continue;
+    // if (!geoID.startsWith("06000")) continue;
 
-      const chunk = [`'${geoId}'`, ...chunkLine];
-      chunks.push(chunk);
+    const queryValues = [`'${geoID}'`, ...selectedValues];
+    valuesBatch.push(queryValues);
 
-      if(chunks.length >= BATCH_SIZE) {
-        i += chunks.length;
-        const q = `
-          insert into data (id, ${columns.join(", ")})
-          values ${chunks.map(chunk => `(${chunk.join(", ")})`).join(",\n  ")}
-          on conflict (id) do update set
-            ${columns.map((col, i) => `"${col}" = excluded."${col}"`).join(",\n  ")}
-        `;
+    if (valuesBatch.length >= BATCH_SIZE) {
+      await insertValuesBatch(valuesBatch, selectedColumns);
+      rows += valuesBatch.length;
+      console.log("  --", `Inserted ${valuesBatch.length} rows for ${filePath}`);
 
-        await connection.run(q);
-        console.log("  --", `Inserted ${chunks.length} rows for ${file}`);
-
-        chunks.length = 0;
-      }
+      valuesBatch.length = 0;
     }
+  }
 
-    console.log("--", `Loaded ${file} with ${i} rows`);
+  console.log("--", `Loaded ${filePath} with ${rows} rows`);
+}
+
+async function parseCsvFile(filePath: string){
+  const fileStream = Bun.file(filePath).stream();
+  const lineStream = createLineStream(fileStream);
+  
+  const firstLine = await lineStream.next();
+  if(!firstLine.value) return;
+  
+  // Select Columns
+  const columnLine = separateLineCSV(firstLine.value);
+  const selectedIndices = new Set<number>();
+  columnLine.forEach((col, index) => {
+    if (col.endsWith("E") && col !== "NAME") selectedIndices.add(index);
+  });
+  
+  const selectedColumns = [...selectedIndices].map(i => columnLine[i]);
+  console.log("--", selectedColumns.length, "columns in", filePath);
+  
+  // Skip label line
+  const _ = lineStream.next();
+  
+  // Insert Rows
+  let rows = 0;
+  const valuesBatch: (string | number)[][] = [];
+  for await (const line of lineStream) {
+    const split = separateLineCSV(line);
+    const geoID = split[0]!;
+    const selectedValues = split.filter((_, idx) => selectedIndices.has(idx)).map(parseNumber);
+      
+    const queryValues = [`'${geoID}'`, ...selectedValues];
+    valuesBatch.push(queryValues);
+
+    if (valuesBatch.length >= BATCH_SIZE) {
+      await insertValuesBatch(valuesBatch, selectedColumns);
+      rows += valuesBatch.length;
+      console.log("  --", `Inserted ${valuesBatch.length} rows for ${filePath}`);
+
+      valuesBatch.length = 0;
+    }
   }
   
+  console.log("--", `Loaded ${filePath} with ${rows} rows`);
 }
 
-export async function loadAll() {
-  const files = await glob(`${BASE_PATH}/**/*.csv`);
+async function insertValuesBatch(valueBatch: (string | number)[][], selectedColumns: (string | undefined)[]) {
+  const q = `
+    insert into data (id, ${selectedColumns.join(", ")})
+    values ${valueBatch.map((chunk) => `(${chunk.join(", ")})`).join(",\n  ")}
+    on conflict (id) do update set
+      ${selectedColumns.map((col, i) => `"${col}" = excluded."${col}"`).join(",\n  ")}
+  `;
 
-  for(const file of files) {
-    const index = files.indexOf(file);
-    console.log(`Loading ${index + 1}/${files.length}: ${file}`);
-    const csv = Bun.file(file).stream();
-    const lineStream = createLineStream(csv);
-
-    const firstLine = await lineStream.next();
-    if(!firstLine.value) continue;
-    const columnLine = separateLine(firstLine.value);
-
-    const keptIndices = new Set<number>();
-
-    columnLine.forEach((col, index) => {
-      if(col.endsWith("E") && col !== "NAME") {
-        keptIndices.add(index);
-      }
-    });
-
-    const columns = [...keptIndices].map(i => columnLine[i]);
-
-    console.log("--", columns.length, "columns in", file);
-
-    // label line
-    const _ = lineStream.next();
-
-    let i = 0;
-    const chunks: (string | number)[][] = [];
-
-    for await(const line of lineStream) {
-      const split = separateLine(line);
-      const chunkLine = split
-        .filter((_, i) => keptIndices.has(i))
-        .map(val => {
-          const asNumber = Number(val);
-          if(isNaN(asNumber)) return "NULL";
-          return asNumber;
-        });
-      const geoId = split[0]! as string;
-
-      chunks.push([`'${geoId}'`, ...chunkLine]);
-
-      if(chunks.length >= BATCH_SIZE) {
-        i += chunks.length;
-        const q = `
-insert into data (id, ${columns.join(", ")})
-values ${chunks.map(chunk => `(${chunk.join(", ")})`).join(",\n  ")}
-on conflict (id) do update set
-  ${columns.map((col, i) => `"${col}" = excluded."${col}"`).join(",\n  ")}
-        `;
-
-        await connection.run(q);
-        console.log("  --", `Inserted ${chunks.length} rows for ${file}`);
-
-        chunks.length = 0;
-      }
-    }
-
-    console.log("--", `Loaded ${file} with ${i} rows`);
-  }
+  await connection.run(q);
 }
-
-// 
 
 function extractEstimateColumns(line: string, fileType: "csv" | "dat") {
   switch (fileType) {
     case "csv":
-      return separateLine(line).filter((id) => id.endsWith("E") && id !== "NAME");
+      return separateLineCSV(line).filter((id) => id.endsWith("E") && id !== "NAME");
     case "dat":
       return separateLineDat(line).filter((id) => id.includes("_E") && id !== "NAME");
   }
@@ -199,4 +173,9 @@ function getFileType(file: string) {
   if (file.endsWith(".csv")) return "csv";
   if (file.endsWith(".dat")) return "dat";
   return "unknown";
+}
+
+function parseNumber(val: string) {
+  const asNumber = Number(val);
+  return isNaN(asNumber) ? "NULL" : asNumber;
 }
